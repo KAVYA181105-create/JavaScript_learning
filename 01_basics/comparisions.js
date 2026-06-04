644a08904c1f9b8bb2eca1d15e037750b0441094
KAVYA181105-create <kavyabisht181105@gmail.com>
2026-06-04 10:01:51 +0000

    comparisions in javascript and more about  datatypes

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,18 @@
+// console.log(2==2);
+// console.log(2!=2);
+// console.log(2>=6);
+// console.log(2<=12);
+// console.log(2>5);
+// console.log(2<3);
+// console.log("02">2);
+// console.log("2">2);
+// console.log("2">100);
+// console.log("2">"100");// how?????? ==> because it compares only the first character as it will be comparing character by cahracter
+// console.log("2">"1");// how?????? same as above
+// console.log("25">"31");// how?????? same as above==> fasle
+// console.log("22">"21");// how?????? same as above==> true
+console.log(null>0);//=>false
+console.log(null==0);//=>false
+console.log(null>=0);//=>true// undefined will give false in all the cases
+// === this is strict check
+console.log("2"===2); //=> false because different datatype
--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -10,6 +10,7 @@ let isLoggedIn = true;//bool
 
 
 /* 
+primitive data types ---- total 7-----
 number => 2^53
 bigint
 string =>" "
@@ -22,6 +23,29 @@ symbol=>unique
 object
 */
 
-console.log(typeof("karan"));//string
-console.log(typeof(null));//obeject
-console.log(typeof(undefined));//undefined
+// console.log(typeof("karan"));//string
+// console.log(typeof(null));//obeject
+// console.log(typeof(undefined));//undefined
+// //JAVASCRIPT IS DYNAMICALLY TYPED
+
+//------reference type (NON PRIMITIVE )--------
+// ARRAYS , OBEJECTS , FUNCTIONS
+
+const id = Symbol("123");
+const Another_id = Symbol("123");
+console.log([id,Another_id,id==Another_id]);
+
+//array
+let HERO = ["karan", "vasu","max","kavya","vidhaan" ];
+//obeject
+let myobj ={
+   name:"karan",
+   roll_number : "24_cs_242",
+   age :20
+}
+
+const my_function = function(){
+    console.log(" hello world /n ")
+}
+
+console.table([typeof(my_function),typeof(myobj),typeof(HERO)]);
