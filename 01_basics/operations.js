89f346dffc44cfe3fc65db8e9d751e2d918fcbd2
KAVYA181105-create <kavyabisht181105@gmail.com>
2026-06-04 09:09:34 +0000

    javascript operartions

---
+++ 01_basics/operations.js
@@ -0,0 +1,29 @@
+let value = 18;
+let NegValue = -value;
+// console.log(NegValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// // console.log(2%3);
+// let str1 = "karan "
+// let str2 = " the greatest of all time "
+// let str3= str1 + str2+str1;
+// console.log(str3);
+
+// console.log("1"+2); //=> 12
+// console.log(1+"2");//=>12
+// console.log("1"+2+18);//=>1218
+// console.log(1+2+"18");//=>318
+
+// console.log(+true) // => 1
+// let num1 , num2 , num3 , num4 ;
+// num1 = num2=num3=num4=3;
+// console.log(num4);
+
+let height = 170;
+
+console.log(height++);//=>170
+console.log(++height);//=>172
+console.log(height++);//=>172
