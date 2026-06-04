5d1450983d4db82b78b38170104ba597850ebec7
KAVYA181105-create <kavyabisht181105@gmail.com>
2026-06-04 08:42:03 +0000

    conversion of datatypes

---
+++ 01_basics/conversioin_problems.js
@@ -0,0 +1,29 @@
+let socre ;
+// console.log(typeof  socre);
+// console.log(typeof(socre));
+
+let valueInNumber = Number(socre);//converts the datatype to NUMBER 
+// console.table([typeof(socre),typeof(valueInNumber)]);
+// console.log(valueInNumber);
+
+/*
+"33"==> easily convert to number =33
+"33ans" ==> dont convert to numeber they give NAN as OUTPUT which has full form not a number
+true ==> 1
+false ==> 0
+null ==> 0
+undefined ==> NAN 
+in NAN it shows as number while seeing datatype but actually its not a number 
+*/
+
+let isLoggedIn =98;
+let booleanIsLoggedin = Boolean(isLoggedIn);
+console.log([typeof(booleanIsLoggedin),booleanIsLoggedin,isLoggedIn])
+/*
+1=> true
+0=>false
+""=>false
+"anything"=> true
+null=> false
+undefined => false
+*/
