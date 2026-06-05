9c7b6bc1f59ea9a734e34822776cf32c2e0ae52e
KAVYA181105-create <kavyabisht181105@gmail.com>
2026-06-05 19:18:11 +0000

    new learning

---
+++ 01_basics/STRING.js
@@ -0,0 +1,17 @@
+let name = "karan"; // this is string
+// let num = 89;
+// console.log(name + num + " hello how are you ? ");
+// console.log(`hello my name is ${name} I like to play ${num} times cricket`);
+const name1 = new String('         KARAN');//this is string object
+// console.log(name1[2]);
+
+// console.log(name1.toLowerCase())
+
+// console.log(name1.length)
+// console.log(typeof(name));
+// // console.log(typeof(name1));
+// console.log(name1.charAt(4));
+// console.log(name1.indexOf('K'));//if element not in string it gives -1
+console.log(name1.substring(1,4));//can be used for positive value only
+console.log(name1.slice(-5,-1));//can be used  for negative as wellas positive value
+console.log(name1.trim());
---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,6 @@
+const num = 45;
+const num1 = new Number(181105)
+console.log(num);
+console.log(num1);
+
+console.log(num1.toFixed)
