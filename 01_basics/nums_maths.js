cd823917232cf0efd65f6e80a1b2d9ecc21532dc
KAVYA181105-create <kavyabisht181105@gmail.com>
2026-06-09 07:56:11 +0000

    math and num

--- 01_basics/nums_maths.js
+++ 01_basics/nums_maths.js
@@ -1,6 +1,27 @@
 const num = 45;
 const num1 = new Number(181105)
-console.log(num);
-console.log(num1);
+// console.log(num);
+// console.log(num1);
+const num2 = new Number(123.125);
+// console.log(num2.toPrecision(4));
+// console.log(num1.toFixed(3));
+const num3 = new Number(12300000);
+// console.log(num3.toLocaleString('en-IN'));
 
-console.log(num1.toFixed)
+//-------------------------******MATHS *********--------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-3243));
+// console.log(Math.round(45.34));
+// console.log(Math.ceil(34.3));
+// console.log(Math.floor(34.3));
+// console.log(Math.max(3,34,53,56,33));
+// console.log(Math.min(34,34,66,343,45,));
+// console.log((Math.random()));
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 20;
+const max =25;
+
+console.log(Math.floor(Math.random()*(max - min +1) + min) );
+
